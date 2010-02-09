edc9f7e075d4ceb0e806955c158de7290ad8203a
Nathan Friedly <nfriedly@titans.dreamhost.com>
2010-02-08 16:54:35 -0800

    added support for multi-term search

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -2,9 +2,15 @@ class Verse < ActiveRecord::Base
   belongs_to :book
 
   def self.search(search, page)
+    conditions = []
+    search.split(/\s+/).map{ |term| 
+      conditions << "text like " + sanitize('%' + term + '%') 
+    }
     paginate :per_page => 20, :page => page,
-           :conditions => ['text like ?', "%#{search}%"]
+           :conditions => conditions.join(" AND ")
   end
+
+
 
 
 end
