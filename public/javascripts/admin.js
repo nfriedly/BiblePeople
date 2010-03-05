c5c923d66ff1f2d070176564b48778d981194308
Nathan Friedly <nfriedly@titans.dreamhost.com>
2010-03-05 07:55:35 -0800

    tweaked family trees, disabled javascript clicker on non-search pages

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,34 +1,39 @@
 $(document).ready(function(){
 
- // create my word-actions div
- $('<div id="word-actions" class="hoverbox"><a href="/people/new" id="create-person">Create Person</a></div>')
-    .appendTo('body');
- var WordActions = $('#word-actions');
- var CPLink = $('#create-person');
- var WAWidth = WordActions.width()
+  // only bother with this if we're on a page with verses
+  if($("p.verse:first").length) {
 
- // placeholder for the current selected word
- var Word;
+     // create my word-actions div
+     $('<div id="word-actions" class="hoverbox"><a href="/people/new" id="create-person">Create Person</a></div>')
+        .appendTo('body');
+     var WordActions = $('#word-actions');
+     var CPLink = $('#create-person');
+     var WAWidth = WordActions.width()
 
- // event when a word is clicked
- $("p.verse span").live('click',function(){
-    if(Word){
-      Word.removeClass('active-word');
-    }
-    Word = $(this);
-    Word.addClass('active-word');
-    
-    // show the actions box
-    var offset = Word.offset();
-    offset.top = offset.top - 2;
-    offset.left = offset.left - (WAWidth - Word.width())/2;
-    WordActions.offset(offset);
+     // placeholder for the current selected word
+     var Word;
 
-    // update the links
-    CPLink.attr('href', '/people/new?name=' + Word.text().replace(/[^a-z ]/i,''));
- });
+     // event when a word is clicked
+     $("p.verse span").live('click',function(){
+        if(Word){
+          Word.removeClass('active-word');
+        }
+        Word = $(this);
+        Word.addClass('active-word');
+        
+        // show the actions box
+        var offset = Word.offset();
+        offset.top = offset.top - 2;
+        offset.left = offset.left - (WAWidth - Word.width())/2;
+        WordActions.offset(offset);
+
+        // update the links
+        CPLink.attr('href', '/people/new?name=' + Word.text().replace(/[^a-z ]/i,''));
+     });
+   
+   }
  
- // autocomplete for the parents
+ // autocomplete for the parents (only runs on pages with #father_name or #mother_name fields)
  $('#father_name, #mother_name').autocomplete(
     "/people/parent", // .json or .xml also work
     {
