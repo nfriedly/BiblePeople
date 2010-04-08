efe5a2826501223c9bee887462a40c16bcd2e878
Nathan Friedly <nfriedly@titans.dreamhost.com>
2010-04-07 17:51:38 -0700

    added basic framework for keyword redirects. (does not actually work yet)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index
+    if(!params[:page]) 
+	redirectUrl = Search.keyword(params[:q])
+	redirect_to redirectUrl if redirectUrl
+    end
     @verses = Search.search(params[:q], params[:page])
     @terms = Search.terms
     @refference = Search.refference
